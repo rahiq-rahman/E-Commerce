b0648f9f201eba8182fce34066ecf43925985bb6
Rahiq Rahman <rahiqrahman2001@gmail.com>
2024-08-22 23:24:37 +0600

    fixed grid issue in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div>
-      <div className='flex flex-col sm:grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm'>
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt="" />
             <p className='w-full md:w-2/3 to-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque odio repellat provident nobis doloremque qui. Similique corporis saepe maxime iste.</p>
@@ -28,11 +28,11 @@ const Footer = () => {
             </ul>
         </div>
 
+      </div>
         <div>
             <hr />
             <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
         </div>
-      </div>
     </div>
   )
 }
